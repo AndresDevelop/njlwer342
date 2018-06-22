89fd8f4e1417908eb2d4f239ee1c9917b06bd952
Yebrail Andres Pacheco Jaime <yebrail.pacheco@globant.com>
2018-06-22 10:52:49 -0500

    Completed

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 /// Modifica el componente para que se puedan agregar tareas
 
 class App extends Component {
+  state = {
+    task: ["Sacar la ropa", "Hacer la cama", "Leer un rato"],
+    newTask: []
+  };
+
+  HandleInput = this.HandleInput.bind(this);
+  HandleKey = this.HandleKey.bind(this);
+
+  HandleInput(e) {
+    this.setState({
+      newTask: e.target.value
+    });
+  }
+
+  HandleKey(e) {
+    if (e.key === "Enter") {
+      e.preventDefault();
+      this.setState({
+        newTask: " "
+      })
+      this.setState({
+        task: this.state.task.concat(this.state.newTask)
+      });
+    }
+  }
+
   render() {
     return (
       <div className="wrapper">
         <div className="list">
           <h3>Por hacer:</h3>
           <ul className="todo">
-            <li>Sacar la ropa</li>
-            <li>Hacer la cama</li>
-            <li>Leer un rato</li>
+            {this.state.task.map((tasks, index) => (
+              <li key={index}>{tasks}</li>
+            ))}
           </ul>
-           <form>
-             <input type="text" id="new-task" placeholder="Ingresa una tarea y oprime Enter" />
-           </form>
+          <form>
+            <input
+              type="text"
+              id="new-task"
+              value={this.state.newTask}
+              placeholder="Ingresa una tarea y oprime Enter"
+              onChange={this.HandleInput}
+              onKeyPress={this.HandleKey}
+            />
+          </form>
         </div>
       </div>
-    )
+    );
   }
 }
-
 
 export default App;
